8290c43fe72f6d13c76204c1df6a620c30499e02
Renato Chea <renato@pizotesoft.com>
2019-09-15 14:45:49 -0600

    fix services v5

--- src/Backend/AdminBundle/Controller/RestController.php
+++ src/Backend/AdminBundle/Controller/RestController.php
@@ -2447,6 +2447,7 @@ class RestController extends FOSRestController
 
 
             ///ADD POINTS
+            $token = $this->get('services')->getBCToken();
             $playerID = $tenantContract->getPlayerId();
             $teamID = $tenantContract->getPropertyContract()->getProperty()->getComplex()->getTeamCorrelative();
             $arrResponse = $this->callGamificationService( "POST", "teams/".$teamID."/players/".$playerID."/pwn/BC-T-00002", array() );
