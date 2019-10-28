4e21675a649879debe2c1a0096e5380c1fd90567
Renato Chea <renato@pizotesoft.com>
2019-10-28 12:11:49 -0600

    fix public ticket v2

--- src/Backend/AdminBundle/Controller/RestController.php
+++ src/Backend/AdminBundle/Controller/RestController.php
@@ -2418,7 +2418,7 @@ class RestController extends FOSRestController
             $description = trim($request->get('description'));
             $photos = $request->get('photos');
             $solution = trim($request->get('solution'));
-            $isPublic = $request->get('is_public') ? 0 : 1;
+            $isPublic = $request->get('is_public');
 
             $categoryId = intval($request->get('category_id'));
             $complexSectorId = intval($request->get('sector_id'));
